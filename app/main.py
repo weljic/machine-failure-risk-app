6a5d99630573f83d22b3056f63063a5f4e2982d6
weljic <veljicvladimirff@gmail.com>
2026-05-24 20:14:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# Ensure parent directory is in Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from model import predict_failure_probability, classify_failure
 
